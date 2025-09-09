dff75626fb94a072c11535e890d3b5c3053cbc7a
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-09-09 21:56:51 +0600

    hero section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from '../components/Home/Hero';
 
 const Home = () => {
     return (
         <div>
-            Home
+            <section>
+                <Hero></Hero>
+            </section>
         </div>
     );
 };
